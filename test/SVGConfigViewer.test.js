67113143902f70dfbfe1102cf435accaea0fbce3
Alexis <schadoc_alex@hotmail.fr>
2016-05-03 14:24:14 +0200

    third version of API

--- test/SVGConfigViewer.test.js
+++ test/SVGConfigViewer.test.js
@@ -1,52 +1,55 @@
 require("mocha").should();
 
 describe("SVGConfigViewer", function() {
-    var obj = {
-        data: {
-            background: {
-                src: ""
-            },
-            door: {
-                opacity: 1,
-                color1: "red",
-                color2: "black"
-            }
+    var cfg = {
+        default: {
+            transition: "none",
+            easing: "ease",
+            duration: 500
         },
-        cfg: {
-            background: {
-                src: {
-                    property: "background-image",
+        background: {
+            src: {
+                property: "background-image",
                     selector: "#background",
                     transition: "none"
-                }
-            },
-            door: {
-                opacity: {
-                    selector: "#door",
+            }
+        },
+        door: {
+            opacity: {
+                selector: "#door",
                     property: "opacity",
                     transition: "fade",
                     easing: "ease",
                     duration: 1000
-                },
-                color1: {
-                    selector: "#door .part1",
-                    property: "fill",
-                    transition: "fade",
-                    easing: "ease",
-                    duration: 500
-                },
-                color2: {
-                    selector: "#door .part2",
-                    property: "background-image",
-                    transition: "fade",
-                    easing: "ease",
-                    duration: 500
-                }
+            },
+            color1: {
+                selector: "#door .part1",
+                property: "fill"
+            },
+            color2: {
+                selector: "#door .part2",
+                property: "fill"
             }
         }
     };
+    var data = {
+        background: {
+            src: ""
+        },
+        door: {
+            opacity: 1,
+            color1: "red",
+            color2: "black"
+        }
+    };
 
-    var viewer = new SVGConfigViewer(obj);
-    viewer.background.src = "http://placehold.it/350x150";
-    viewer.door.color1 = "blue";
+    // Create the viewer with the data and the config (no animation)
+    var viewer = new SVGConfigViewer(data, cfg);
+
+    // Change data the way you want
+    data.background.src = "http://placehold.it/350x150";
+    data.door.color1 = "blue";
+
+    // Change new state with config animations
+    viewer.setState(data);
 });
